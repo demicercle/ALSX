264b2a03f4e4815dd2fa247b920c61b6a10b90f6
PierreGD <pierre.gaudillere@gmail.com>
2022-11-16 07:36:10 +0100

    basic inventory logic

---
+++ Source/ALSX/ALSXGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ALSXGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALSX_API AALSXGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
